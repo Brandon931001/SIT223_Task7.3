62bfed250d18062a05cfd929fb431dfce465e464
Brandon <brandon@skysvr.com>
2025-05-25 14:44:38 +1000

    Add tests folderr with pytest cases

---
+++ tests/test_garage.py
@@ -0,0 +1,19 @@
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from app import Vehicle, Garage, add_vehicle
+
+
+def test_add_vehicle_success():
+    garage = Garage("TestGarage", 2)
+    vehicle = Vehicle("TestCar")
+    assert add_vehicle(garage, vehicle) == True
+    assert len(garage.vehicles) == 1
+
+def test_add_vehicle_failure_when_full():
+    garage = Garage("SmallGarage", 1)
+    vehicle1 = Vehicle("Car1")
+    vehicle2 = Vehicle("Car2")
+    add_vehicle(garage, vehicle1)
+    assert add_vehicle(garage, vehicle2) == False
